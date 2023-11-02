73359822cd94499a091610e74b2acf837b93ab19
The Akash Mistry <39212376+akashmistry@users.noreply.github.com>
2023-11-02 20:55:56 +0530

    Ui for UberEats

---
+++ screens/Home.js
@@ -0,0 +1,21 @@
+import { View, Text, SafeAreaView, ScrollView } from "react-native";
+import React from "react";
+import HeaderTabs from "../components/HeaderTabs";
+import SearchBar from "../components/SearchBar";
+import Categories from "../components/Categories";
+import RestaurantItems from "../components/RestaurantItems";
+
+export default function Home() {
+  return (
+    <SafeAreaView style={{ backgroundColor: "#eee", flex: 1 }}>
+      <View style={{ backgroundColor: "white", padding: 15 }}>
+        <HeaderTabs />
+        <SearchBar />
+      </View>
+      <ScrollView showsVerticalScrollIndicator={false}>
+        <Categories />
+        <RestaurantItems />
+      </ScrollView>
+    </SafeAreaView>
+  );
+}
